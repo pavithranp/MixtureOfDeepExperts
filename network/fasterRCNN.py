8e10c990d078882eb0a89b6008c5359689e294c3
pavithranp <ptpn94@gmail.com>
2021-01-14 20:50:55 +0100

    first commit

---
+++ network/fasterRCNN.py
@@ -0,0 +1,27 @@
+# import some common libraries
+import numpy as np
+import os, json, cv2, random, torch
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.modeling import build_model
+
+def load_saveFasterRCNN():
+    PATH = 'model/faster-RCNN.pth'
+    cfg = get_cfg()
+    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
+    model = build_model(cfg)
+    torch.save(model, PATH)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    load_saveFasterRCNN()
+
