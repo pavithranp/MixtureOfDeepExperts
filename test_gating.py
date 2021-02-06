30358372537dcbeb21923da8d2f62762a4195aa8
pavithranp <ptpn94@gmail.com>
2021-02-06 18:30:43 +0100

    eval

--- test_gating.py
+++ test_gating.py
@@ -1,14 +1,15 @@
-from network.GatingNetwork import  GatingNetwork
-from network.GatingNetwork import Gating_OutputLayer,Gating_ROIHeads
-from detectron2.config import get_cfg
+import cv2
+import detectron2.data.transforms as T
+import torch
 from detectron2 import model_zoo
-import torch,cv2
-import detectron2.data.transforms as T
-from detectron2.modeling import build_model
-import detectron2.data.transforms as T
-from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import ColorMode
+from detectron2.utils.visualizer import Visualizer
 
-def image_process(path1,path2,cfg):
+from network.GatingNetwork import GatingNetwork
+
+
+def image_process(path1, path2, cfg):
     aug = T.ResizeShortestEdge([cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST)
     image1 = cv2.imread(path1)
     image1 = cv2.resize(image1, (1920, 1080))
@@ -23,7 +24,8 @@ def image_process(path1,path2,cfg):
     # image2 = torch.as_tensor(image2.astype("float32").transpose(2, 0, 1))
     image1 = torch.as_tensor(image1.transpose(2, 0, 1), dtype=torch.float32)
     image2 = torch.as_tensor(image2.transpose(2, 0, 1), dtype=torch.float32)
-    return [{"rgb_image": image1,"depth_image": image2, "height": height, "width": width}]
+    return [{"rgb_image": image1, "depth_image": image2, "height": height, "width": width}]
+
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_C4_3x.yaml"))
@@ -34,30 +36,26 @@ cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_C4_3x.yaml")
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 5000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+cfg.SOLVER.MAX_ITER = 5000  # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  # faster, and good enough for this toy dataset (default: 512)
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
-
-# cfg.MODEL.WEIGHTS = "output_RGB/model_final.pth"
-
-# model = build_model(cfg)
 cfg2 = cfg.clone()
 cfg.MODEL.WEIGHTS = "output/model_0024999.pth"
 cfg2.MODEL.WEIGHTS = "output/model_0019999.pth"
-rgb = image_process('docs/imagehd.png','docs/depthJet.png',cfg)
-# depth = image_process(,cfg2)
-# model.eval()
+rgb = image_process('docs/imagehd.png', 'docs/depthJet.png', cfg)
 with torch.no_grad():
-#     pred = model(input)[0]
+    gn = GatingNetwork(cfg, cfg2)
 
-    # RGBD_network = Gating_ROIHeads(cfg,cfg2)
-    # # cfg.MODEL.WEIGHTS = "output/model_final.pth"
-    # Depth_network = FRCNN_ROIHeads(cfg)
-    # x = RGBD_network(rgb,depth)
-    gn = GatingNetwork(cfg,cfg2)
-    # depth = image_process('docs/input.jpg',cfg)
-    # rgb = image_process('docs/input.jpg',cfg)
-    # for i in range(5):
-    print(gn(rgb))
+    outputs = gn(rgb)[0]
+    im = cv2.imread('docs/imagehd.png')
+    image1 = cv2.resize(im, (1920, 1080))
 
-
+    v = Visualizer(image1,
+                   scale=0.5,
+                   instance_mode=ColorMode.IMAGE_BW
+                   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+                   )
+    print(outputs)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imshow('test', out.get_image()[:, :, ::-1])
+    cv2.waitKey(0)
--- viz_singleExpert.py
+++ viz_singleExpert.py
@@ -1,25 +1,31 @@
 import random
-# import some common detectron2 utilities
-from detectron2.utils.visualizer import Visualizer
+
 import cv2
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
+# import some common detectron2 utilities
+from detectron2.utils.visualizer import Visualizer
+
 from dataloader.dataset import Dataset
 
-if __name__=="__main__":
-    # args="ImagesQ_hd/" # change to ImagesQhd/
-    args = "DepthJetQhd/"
+if __name__ == "__main__":
+    args = "ImagesQ_hd/"  # change to ImagesQhd/
+    # args = "DepthJetQhd/"
     x = Dataset(args)
     cfg = get_cfg()
     model = "COCO-Detection/faster_rcnn_R_50_C4_3x.yaml"
     cfg.merge_from_file(model_zoo.get_config_file(model))
     cfg.DATASETS.TRAIN = ("InOutDoorDepth_train",)
     print("InOutDoorDepth_train")
+    cfg.MODEL.RPN.PRE_NMS_TOPK_TRAIN = 3000
+    cfg.MODEL.RPN.PRE_NMS_TOPK_TEST = 3000
+    cfg.MODEL.RPN.POST_NMS_TOPK_TRAIN = 500
+    cfg.MODEL.RPN.POST_NMS_TOPK_TEST = 500
     cfg.DATASETS.TEST = ()
     cfg.OUTPUT_DIR = args
     cfg.DATALOADER.NUM_WORKERS = 2
-    cfg.MODEL.WEIGHTS = 'DepthJetQhd/model_final.pth'
+    cfg.MODEL.WEIGHTS = 'output/model_0024999.pth'
     cfg.SOLVER.IMS_PER_BATCH = 2
     cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
     cfg.SOLVER.MAX_ITER = 10000  # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
@@ -32,8 +38,11 @@ if __name__=="__main__":
     dataset_dicts = x.load_annotations()
     for d in random.sample(dataset_dicts, 1):
         # im = cv2.imread(d["file_name"])
-        im = cv2.imread('/mnt/AAB281B7B2818911/datasets/InOutDoorPeopleRGBD/DepthJetQhd/seq_1454085791.0435234190.png')
+        im = cv2.imread('docs/imagehd.png')
+        im = cv2.resize(im, (1920, 1080))
+        # im = cv2.imread('/mnt/AAB281B7B2818911/datasets/InOutDoorPeopleRGBD/DepthJetQhd/seq_1454085791.0435234190.png')
         outputs = predictor(im)
+
         # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
         v = Visualizer(im[:, :, ::-1],
                        metadata=x.meta,
